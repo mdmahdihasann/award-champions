3fc1b48b8b236afd2af4b542697d0b84ae3c29e3
mdmahdihasann <mdmahdicmt123@gmail.com>
2026-01-31 12:54:13 +0600

    updated

--- src/components/brand/BrandTable.jsx
+++ src/components/brand/BrandTable.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useAuth } from "../../hooks/useAuth";
+import { useAuth } from "@/hooks/useAuth";
 import FirstPlace from "../../../public/price/medal.png";
 import Image from "next/image";
 import { ContentLoading } from "../Loading";
