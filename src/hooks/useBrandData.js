437b2f82f83d5a727a54ff6238dcdc8350ecd280
mdmahdihasann <mdmahdicmt123@gmail.com>
2026-01-31 15:42:35 +0600

    useAuth file change

--- src/hooks/useBrandData.js
+++ src/hooks/useBrandData.js
@@ -1,7 +1,7 @@
 "use client"
 
 import { useEffect, useState } from "react";
-import { useAuth } from "@/hooks/useAuth"
+import { useAuth } from "../hooks/useAuth"
 import axios from "axios";
 
 const useBrandData = () => {
